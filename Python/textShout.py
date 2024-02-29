3a803e16f13316e64f818cb1aa161f5a8610943a
F0RTY <xaviermontane3@gmail.com>
2024-02-29 12:42:37 +0100

    Changes to be committed: 	modified:   Python/textShout.py

--- Python/textShout.py
+++ Python/textShout.py
@@ -1,1 +1,12 @@
-import os
+with open('sample.txt', 'r') as f:
+    textContent = f.read()
+    whitespaceIndex = 0
+
+    for x in range(len(textContent)):
+        if textContent[x].isspace():
+            print(textContent[whitespaceIndex:x].upper())
+            whitespaceIndex = x + 1
+            
+    # Print the remaining text after the last whitespace (if any)
+    if whitespaceIndex < len(textContent):
+        print(textContent[whitespaceIndex:].upper())
